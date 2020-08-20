097ccda11bf8f8c4913c6d5d20293c6afd018258
Raj Patel <patelraj1677@gmail.com>
2020-08-20 23:02:09 +0530

    Add files via upload
    This python program gets the wifi password for all the saved network on your system. It uses a library named subprocess to interact with cmd, where it used netsh wlan show profiles to get all the profiles and their password.

---
+++ wifipass.py
@@ -0,0 +1,14 @@
+import subprocess
+import pdb
+
+pdb.set_trace()
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        print("{:<20}|  {:<}".format(i, results[0]))
+    except IndexError:
+        print("{:<20}|  {:<}".format(i, ""))
+
